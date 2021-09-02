a4c6588543086630344e8caeb7d7f530ccd1a0c4
Benoît Gelineau <bgelineau.dev@gmail.com>
2021-09-02 11:36:49 +0200

    Style topics list

--- src/components/TopicsList.tsx
+++ src/components/TopicsList.tsx
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom';
 import topics from '@config/topics';
+import { NavLink } from 'react-router-dom';
 
 const TopicsList = () => {
 	return (
 		<div id='topics'>
 			{topics.map(({ name }) => (
-				<Link key={name} to={`/${name}`}>
-					#{name}
-				</Link>
+				<NavLink key={name} to={`/${name}`} activeClassName='active'>
+					<span>#</span>
+					{name}
+				</NavLink>
 			))}
 		</div>
 	);
